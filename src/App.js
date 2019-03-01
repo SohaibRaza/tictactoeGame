184909de708e9dc0b993176ad8de74f819057121
Muhammad Sohaib Raza <sohaibraza000@gmail.com>
2019-03-01 23:54:08 +0500

    First Version

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
+import TicTacToeBoard from './components/TicTacToeBoard'
+import './App.css';
+
+
+function IsVictory(cells) {
+  const positions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let pos of positions) {
+    const symbol = cells[pos[0]];
+    let winner = symbol;
+    for (let i of pos) {
+      if (cells[i] != symbol) {
+        winner = null;
+        break;
+      }
+    }
+    if (winner != null) return true;
+  }
+
+  return false;
+  
+}
+
+// Return true if all `cells` are occupied.
+function IsDraw(cells) {
+  return cells.filter(c => c === null).length == 0;
+}
+
+const TicTacToe = Game({
+  setup: () => ({ cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell(G, ctx, id) {
+      // Ensure that we can't overwrite cells.
+      if (G.cells[id] === null) {
+        G.cells[id] = ctx.currentPlayer;
+      }
+    },
+  },
+
+  flow: {
+    endGameIf: (G, ctx) => {
+      if (IsVictory(G.cells)) {
+        return { winner: ctx.currentPlayer };
+      }
+      if (IsDraw(G.cells)) {
+        return { draw: true };
+      }
+    },
+  },
+});
+
+const App = Client({ game: TicTacToe ,board: TicTacToeBoard,});
+
+export default App;
