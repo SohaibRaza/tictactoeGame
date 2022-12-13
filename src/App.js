aed5a404a9331c926bb4a6a7c53dc1e65b795d4a
SohaibRaza <sohaibraza000@gmail.com>
2022-12-13 20:30:31 +0500

    upgraded dependencies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
 import { Client } from 'boardgame.io/react';
-import { Game } from 'boardgame.io/core';
-import TicTacToeBoard from './components/TicTacToeBoard'
-import { AI } from 'boardgame.io/ai';
-import './App.css';
-import './board.css'
-import TicTacToe from './components/game'
+import TicTacToeBoard from "./components/TicTacToeBoard";
+import TicTacToe from "./components/game";
 
-const App = Client({ 
-  game: TicTacToe ,
+import "./App.css";
+
+const App = Client({
+  game: TicTacToe,
   board: TicTacToeBoard,
-  ai: AI({
-    enumerate: G => {
+  ai: {
+    enumerate: (G) => {
       let moves = [];
       for (let i = 0; i < 9; i++) {
         if (G.cells[i] === null) {
-          moves.push({ move: 'clickCell', args: [i] });
+          moves.push({ move: "clickCell", args: [i] });
         }
       }
       return moves;
-    }
-  })
-
+    },
+  },
 });
 
 export default App;
--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { Client } from 'boardgame.io/react';
-import { Game } from 'boardgame.io/core';
+import { INVALID_MOVE } from "boardgame.io/core";
 
 function IsVictory(cells) {
   const positions = [
@@ -11,52 +9,48 @@ function IsVictory(cells) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
 
-  for (let pos of positions) {
-    const symbol = cells[pos[0]];
-    let winner = symbol;
-    for (let i of pos) {
-      if (cells[i] != symbol) {
-        winner = null;
-        break;
-      }
-    }
-    if (winner != null) return true;
-  }
+  const isRowComplete = (row) => {
+    const symbols = row.map((i) => cells[i]);
+    return symbols.every((i) => i !== null && i === symbols[0]);
+  };
 
-  return false;
-  
+  return positions.map(isRowComplete).some((i) => i === true);
 }
 
 // Return true if all `cells` are occupied.
 function IsDraw(cells) {
-  return cells.filter(c => c === null).length == 0;
+  return cells.filter((c) => c === null).length === 0;
 }
 
-const TicTacToe = Game({
+const TicTacToe = {
   setup: () => ({ cells: Array(9).fill(null) }),
 
   moves: {
-    clickCell(G, ctx, id) {
-      // Ensure that we can't overwrite cells.
-      if (G.cells[id] === null) {
-        G.cells[id] = ctx.currentPlayer;
+    clickCell({ G, playerID }, id) {
+      if (G.cells[id] !== null) {
+        return INVALID_MOVE;
       }
+
+      G.cells[id] = playerID;
     },
   },
 
-  flow: {
-    endGameIf: (G, ctx) => {
-      if (IsVictory(G.cells)) {
-        return { winner: ctx.currentPlayer };
-      }
-      if (IsDraw(G.cells)) {
-        return { draw: true };
-      }
-    },
+  turn: {
+    minMoves: 1,
+    maxMoves: 1,
   },
-});
 
-export default TicTacToe
+  endIf: ({ G, ctx }) => {
+    if (IsVictory(G.cells)) {
+      return { winner: ctx.currentPlayer };
+    }
+    if (IsDraw(G.cells)) {
+      return { draw: true };
+    }
+  },
+};
+
+export default TicTacToe;
